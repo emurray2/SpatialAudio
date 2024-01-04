10ce8256a35d419480e6a4b09abf37fa3e3c1279
Evan Murray <evan@auraaudio.io>
2024-01-04 11:40:25 -0600

    Initial Commit

---
+++ SpatialAudio/SpatialAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialAudioApp.swift
+//  SpatialAudio
+//
+//  Created by Aura Audio on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
